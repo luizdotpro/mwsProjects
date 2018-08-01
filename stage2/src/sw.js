1ddf7a9e22069bcf8e15fa782c6c5c11c12c2d95
Luiz Carneiro <luuizpaulo@gmail.com>
2018-08-01 14:56:13 +0200

    manifest, service worker, gulp added and modified

--- stage2/src/sw.js
+++ stage2/src/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('stage1').then(cache => {
+    caches.open('stage2').then(cache => {
       return cache.addAll([
         '/',
         'index.html',
@@ -8,7 +8,17 @@ self.addEventListener('install', event => {
         '/css/styles.min.css',
         '/js/dbhelper.js',
         '/js/main.js',
-        '/data/restaurants.json',
+        '/js/restaurant_info.js',
+        '/img/1_w_500.jpg',
+        '/img/2_w_500.jpg',
+        '/img/3_w_500.jpg',
+        '/img/4_w_500.jpg',
+        '/img/5_w_500.jpg',
+        '/img/6_w_500.jpg',
+        '/img/7_w_500.jpg',
+        '/img/8_w_500.jpg',
+        '/img/9_w_500.jpg',
+        '/img/10_w_500.jpg',
       ]);
     })
   )
